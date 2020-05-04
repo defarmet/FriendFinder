80a1be0e771d067cab3f7886bf307270fb9e8e74
defarmet <defarmet@protonmail.com>
2020-05-03 23:58:26 -0400

    Added home page and survey backend code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,1 @@
-var friends = [];
+module.exports = [];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,24 @@
 var friends = require("../data/friends.js");
 
+var current = {};
+var best = {
+	total: Number.MAX_VALUE,
+	friend: {}
+};
+
+function get_best(value)
+{
+	var total = 0;
+	for (var i = 0; i < 10; i++) {
+		total += Math.abs(value.scores[i] - current.scores[i]);
+	}
+
+	if (total < best.total) {
+		best.total = total;
+		best.friend = value;
+	}
+}
+
 module.exports = function(app)
 {
 	app.get("/api/friends", function(req, res)
@@ -9,6 +28,19 @@ module.exports = function(app)
 
 	app.post("/api/friends", function(req, res)
 	{
-		friends.push(req.body);
+		best = {
+			total: Number.MAX_VALUE,
+			friend: {}
+		};
+
+		if (friends.length < 1) {
+			friends.push(req.body);
+			return res.json("NOT ENOUGH USERS");
+		}
+
+		current = req.body;
+		friends.forEach(get_best);
+		friends.push(current);
+		return res.json(best);
 	});
 }
